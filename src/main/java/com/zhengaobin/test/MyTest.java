3125e4f048b812b3304aaa231fadf2a9c42d6e7b
zhengaobinlonglive <2484730276@qq.com>
2019-10-11 09:08:41 +0800

    第一次添加

---
+++ src/main/java/com/zhengaobin/test/MyTest.java
@@ -0,0 +1,15 @@
+package com.zhengaobin.test;
+
+/**
+ * @author 郑奥斌
+ *
+ * 2019年10月11日
+ */
+public class MyTest {
+	public static void main(String[] args) {
+		
+		System.out.println("this is test");
+		
+		
+	}
+}
